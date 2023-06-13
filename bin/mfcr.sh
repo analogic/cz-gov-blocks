ebbf84f37ed931a53712c88ab854eec8467eb9f7
SH <sh@analogic.cz>
2023-06-13 15:02:54 +0200

    prevent mfcr errors

--- bin/mfcr.sh
+++ bin/mfcr.sh
@@ -3,14 +3,24 @@
 set -ex
 
 # naively find link to the article
-article=`wget --inet4-only -q https://www.mfcr.cz/cs/soukromy-sektor/hazardni-hry/seznam-nepovolenych-internetovych-her -O - | perl -lne 'print $1 if /a href="(.*?seznam.*?)"/' | head -n 1`
+article=`wget https://www.mfcr.cz/cs/soukromy-sektor/hazardni-hry/seznam-nepovolenych-internetovych-her -O - | perl -lne 'print $1 if /a href="(.*?seznam.*?)"/' | head -n 1`
+if [ -z "$article" ]
+then
+    echo "parser error"
+    exit 1
+fi
 article="https://www.mfcr.cz${article}"
 
 # find link to csv
-file=`wget --inet4-only -q ${article} -O - | perl -lne 'print $1 if /a href="(.*?\.csv.*?)"/' | head -n 1`
+file=`wget ${article} -O - | perl -lne 'print $1 if /a href="(.*?\.csv.*?)"/' | head -n 1`
+if [ -z "$file" ]
+then
+    echo "parser error"
+    exit 1
+fi
 file="https://www.mfcr.cz${file}"
 
-wget --inet4-only -q "${file}" -O original/mfcr.csv
+wget "${file}" -O original/mfcr.csv
 
 cat original/mfcr.csv | tail -n +2 | awk -F";" '{print $1}' > csv/mfcr.csv
 

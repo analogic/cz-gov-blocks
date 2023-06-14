c6b1cb89656533e2d2bcbf2bc88e208ebc551ee5
SH <sh@analogic.cz>
2023-06-14 09:53:52 +0200

    force ipv4 for mfcr

--- bin/mfcr.sh
+++ bin/mfcr.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # naively find link to the article
-article=`wget https://www.mfcr.cz/cs/soukromy-sektor/hazardni-hry/seznam-nepovolenych-internetovych-her -O - | perl -lne 'print $1 if /a href="(.*?seznam.*?)"/' | head -n 1`
+article=`wget --inet4-only https://www.mfcr.cz/cs/soukromy-sektor/hazardni-hry/seznam-nepovolenych-internetovych-her -O - | perl -lne 'print $1 if /a href="(.*?seznam.*?)"/' | head -n 1`
 if [ -z "$article" ]
 then
     echo "parser error"
@@ -12,7 +12,7 @@ fi
 article="https://www.mfcr.cz${article}"
 
 # find link to csv
-file=`wget ${article} -O - | perl -lne 'print $1 if /a href="(.*?\.csv.*?)"/' | head -n 1`
+file=`wget --inet4-only ${article} -O - | perl -lne 'print $1 if /a href="(.*?\.csv.*?)"/' | head -n 1`
 if [ -z "$file" ]
 then
     echo "parser error"
@@ -20,7 +20,7 @@ then
 fi
 file="https://www.mfcr.cz${file}"
 
-wget "${file}" -O original/mfcr.csv
+wget --inet4-only "${file}" -O original/mfcr.csv
 
 cat original/mfcr.csv | tail -n +2 | awk -F";" '{print $1}' | sed 's/ //g' > csv/mfcr.csv
 

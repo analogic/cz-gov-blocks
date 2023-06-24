66d4b1a6033602036e7b22012d20ecc12b5282c7
analogic <info@analogic.cz>
2023-06-24 08:33:46 +0200

    USKVBL moved to CSV finally

--- bin/uskvbl.sh
+++ bin/uskvbl.sh
@@ -5,11 +5,9 @@ set -ex
 article="https://uskvbl.cz/cs/inspekce/nelegalni-vlp"
 
 # find link to csv
-file=`wget -q ${article} -O - | perl -lne 'print $1 if /href="(.*?\.xlsx)"/'`
+file=`wget -q ${article} -O - | perl -lne 'print $1 if /href="(.*?\.csv)"/'`
 
-wget -q "${file}" -O original/uskvbl.xlsx
+wget -q "${file}" -O original/uskvbl.csv
 
-pip install xlsx2csv
-
-xlsx2csv original/uskvbl.xlsx | tail -n +2 | awk -F"," '{print $1}' | sed 's/ //g' > csv/uskvbl.csv
+cat original/uskvbl.csv | tail -n +2 | awk -F"," '{print $1}' | sed 's/ //g' > csv/uskvbl.csv
 

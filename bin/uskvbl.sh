c3b94f7663b6540ef80eb14a3ecb6f1b86b6177c
analogic <info@analogic.cz>
2025-08-17 09:28:04 +0200

    Novy format uskvbl.sh

--- bin/uskvbl.sh
+++ bin/uskvbl.sh
@@ -9,9 +9,15 @@ file=`wget -q ${article} -O - | perl -lne 'print $1 if /href="(.*?\.csv)"/'`
 wget -q "${file}" -O original/uskvbl.csv
 
 cat original/uskvbl.csv \
-    | tail -n +2 \
-    | awk -F";" 'length($3) <= 2 {print $1}' \
-    | sed 's/ //g' \
-    | awk NF \
-> csv/uskvbl.csv
+    | awk -F',' 'NR>1 {
+  vymaz=$3
+  gsub(/^[ \t\r]+|[ \t\r]+$/,"",vymaz)          # ořež bílé znaky
+  if (vymaz=="") {
+    u=$1
+    gsub(/^[ \t\r]+|[ \t\r]+$/,"",u)            # ořež bílé znaky
+    sub(/^https?:\/\//,"",u)                    # smaž http/https
+    split(u, a, "/")                             # odděl cestu
+    print a[1]                                   # vytiskni host (doménu)
+  }
+}' > csv/uskvbl.csv
 

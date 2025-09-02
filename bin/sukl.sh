524a99bd2f416cba0a1c665219e1485730f5ad21
analogic <info@analogic.cz>
2025-09-02 06:41:37 +0200

    ignore bad sukl cert

--- bin/sukl.sh
+++ bin/sukl.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-wget -q https://opendata.sukl.cz/soubory/NELEGALNI_STRANKY/Nelegalni_stranky_LP.csv -O original/sukl.csv
+wget --no-check-certificate -q https://opendata.sukl.cz/soubory/NELEGALNI_STRANKY/Nelegalni_stranky_LP.csv -O original/sukl.csv
 
 cat original/sukl.csv \
   | tail -n +2 \
